680eca25ec9232afdffb21d19c64b2945abaabda
leo9224 <leofetter92@gmail.com>
2023-04-24 08:52:46 +0200

    fix index tests bugs

--- tests/CiviliteDao.test.ts
+++ tests/CiviliteDao.test.ts
@@ -1,11 +1,59 @@
 import {Civilite} from "../models/Civilite";
 import {CiviliteService} from "../services/CiviliteService";
+import {PrismaClient} from "@prisma/client";
 
 let civilite1: Civilite
 let civilite2: Civilite
 let civilite3: Civilite
 
-beforeEach(async () => {
+const prisma_client = new PrismaClient()
+
+beforeAll(async () => {
+    await prisma_client.civilite.create({
+        data: {
+            id_civilite: 1,
+            libelle: "Mr"
+        }
+    })
+
+    await prisma_client.civilite.create({
+        data: {
+            id_civilite: 2,
+            libelle: "Mme"
+        }
+    })
+
+    await prisma_client.civilite.create({
+        data: {
+            id_civilite: 3,
+            libelle: "Mlle"
+        }
+    })
+})
+
+afterAll(async () => {
+    const prisma_client = new PrismaClient()
+
+    await prisma_client.civilite.delete({
+        where: {
+            id_civilite: 1
+        }
+    })
+
+    await prisma_client.civilite.delete({
+        where: {
+            id_civilite: 2
+        }
+    })
+
+    await prisma_client.civilite.delete({
+        where: {
+            id_civilite: 3
+        }
+    })
+});
+
+beforeEach(() => {
     civilite1 = new Civilite(1, "Mr")
     civilite2 = new Civilite(2, "Mme")
     civilite3 = new Civilite(3, "Mlle")
